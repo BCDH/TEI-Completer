48ecfb6656861792545759d294a14b9fa9c809ea
marmoure <tinky93winky@gmail.com>
2023-11-14 20:57:35 +0100

    [fix] add missing license

--- src/main/java/org/humanistika/oxygen/tei/completer/GUI/newSuggestionForm.java
+++ src/main/java/org/humanistika/oxygen/tei/completer/GUI/newSuggestionForm.java
@@ -1,6 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
+/**
+ * TEI Completer
+ * An Oxygen XML Editor plugin for customizable attribute and value completion for TEI P5 documents
+ * Copyright (C) 2016 Belgrade Center for Digital Humanities
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package org.humanistika.oxygen.tei.completer.GUI;
 
